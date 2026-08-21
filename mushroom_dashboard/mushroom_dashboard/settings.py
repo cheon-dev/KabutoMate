59911a9e4b4c9106ad1cb3a91c7b4863b9e2763c
Dan Kirvy Manongsong <dankirvymanongsong@gmail.com>
2026-08-21 13:09:01 +0800

    Switch to Supabase transaction pooler, disable persistent connections

--- mushroom_dashboard/mushroom_dashboard/settings.py
+++ mushroom_dashboard/mushroom_dashboard/settings.py
@@ -148,7 +148,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True),
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=0, ssl_require=True),
     }
 else:
     DATABASES = {

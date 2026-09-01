5f9f342537f2ca20533989966e40bc539bf30faa
Dan Kirvy Manongsong <dankirvymanongsong@gmail.com>
2026-09-01 17:32:23 +0800

    Add password reset email button

--- mushroom_dashboard/core/urls.py
+++ mushroom_dashboard/core/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('forgot-password/', views.SitePasswordResetView.as_view(
         template_name='password_reset_form.html',
-        email_template_name='registration/password_reset_email.html',
+        email_template_name='registration/password_reset_email.txt',
+        html_email_template_name='registration/password_reset_email.html',
         subject_template_name='registration/password_reset_subject.txt',
         success_url=reverse_lazy('password_reset_done'),
     ), name='password_reset'),
